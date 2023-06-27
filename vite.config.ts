31e09aef249ac5bc8ccf92c38e7a3940d9f3c4e9
Nguyễn Tài <dinhtai040197@gmail.com>
2023-06-27 03:52:26 +0300

    add type to mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
-export default ({ mode }) => {
+export default (mode: any) => {
   // Load app-level env vars to node-level env vars.
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
